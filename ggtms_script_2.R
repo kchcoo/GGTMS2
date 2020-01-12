b2069238544e6c827bd450b2ecf5f292847e3f9c
kchcoo <kchcoo@gg.go.kr>
2020-01-13 00:17:04 +0900

    다차원배열 생성 필요

--- ggtms_script_2.R
+++ ggtms_script_2.R
@@ -33,6 +33,26 @@ test_data <- raw_1904_1910_5 #다른 테스트를 위한 복사본
 test_PM10 <- select(test_data, DATA_DT, LOC_CODE, PM10) #일자, 측정소번호, PM10만 남기고 잘라냄
 #5분 단위의 시간이 row가 되고 각 측정소의 측정소번호가 column이 됨
 test_PM10_2 <- pivot_wider(test_PM10, names_from = LOC_CODE, values_from = PM10)
+test_PM2.5 <- pivot_wider(test_data, names_from = LOC_CODE, values_from = PM2.5)
+test_etc <- pivot_wider(test_data, names_from = LOC_CODE, values_from = c(PM2.5, PM10))
+
+#리스트가 문제가 아니라 그냥 다차원 배열을 만들면 되는것이었음 다만 이걸 파이썬에서 할지 여기서 해서 내보내야 하는지 고민
+#일단 여기서 3차원 배열을 만들어보기로 함
+
+pivot_PM10 <- select(test_data, DATA_DT, LOC_CODE, PM10) %>% pivot_wider(names_from = LOC_CODE, values_from = PM10)
+pivot_PM2.5 <- select(test_data, DATA_DT, LOC_CODE, PM2.5) %>% pivot_wider(names_from = LOC_CODE, values_from = PM2.5)
+pivot_O3 <- select(test_data, DATA_DT, LOC_CODE, O3) %>% pivot_wider(names_from = LOC_CODE, values_from = O3)
+pivot_CO <- select(test_data, DATA_DT, LOC_CODE, CO) %>% pivot_wider(names_from = LOC_CODE, values_from = CO)
+pivot_SO2 <- select(test_data, DATA_DT, LOC_CODE, SO2) %>% pivot_wider(names_from = LOC_CODE, values_from = SO2)
+pivot_NO2 <- select(test_data, DATA_DT, LOC_CODE, NO2) %>% pivot_wider(names_from = LOC_CODE, values_from = NO2)
+pivot_NOX <- select(test_data, DATA_DT, LOC_CODE, NOX) %>% pivot_wider(names_from = LOC_CODE, values_from = NOX)
+pivot_NO <- select(test_data, DATA_DT, LOC_CODE, NO) %>% pivot_wider(names_from = LOC_CODE, values_from = NO)
+pivot_WD <- select(test_data, DATA_DT, LOC_CODE, WD) %>% pivot_wider(names_from = LOC_CODE, values_from = WD)
+pivot_WS <- select(test_data, DATA_DT, LOC_CODE, WS) %>% pivot_wider(names_from = LOC_CODE, values_from = WS)
+pivot_TMP <- select(test_data, DATA_DT, LOC_CODE, TMP) %>% pivot_wider(names_from = LOC_CODE, values_from = TMP)
+pivot_HUM <- select(test_data, DATA_DT, LOC_CODE, HUM) %>% pivot_wider(names_from = LOC_CODE, values_from = HUM)
+
+pivot <- list(pivot_PM10, pivot_PM2.5, pivot_O3, pivot_CO, pivot_SO2, pivot_NO2, pivot_NOX, pivot_NO, pivot_WD, pivot_WS, pivot_TMP, pivot_HUM)
 
 #위의 형태로 데이터를 나타내야하는 이유는 전처리된 raw_1904_1910의 형태가 측정시간과 측정소번호에 따라 중복되는 시간 자료가 있기 때문
 #모든 시간 자료는 하나만 있어야 함, 측정물질이 달라지면 다른 테이블의 형태로 다른 channel로 구성되야 한다고 판단됨
@@ -58,3 +78,18 @@ test_PM10_4 <- dplyr::left_join(test_PM10_4, test_PM10_3, by = c("lag_DATA_DT" =
 test_PM10_3 <- filter(test_PM10_3, DATA_DT >= 201904020000) #타겟 데이터를 만들고나서 맞는 타겟 데이터가 없는 날짜범위를 지움
 test_PM10_4 <- filter(test_PM10_4, lag_DATA_DT <= 201910312355)
 test_PM10_4 <- test_PM10_4[-147,] #147번째 줄에서 5분단위가 아닌 시간 행이 발견되어 삭제 refactory할 때 5분단위 자료 검증코드도 넣을것
+
+#위에서 PM10을 대상으로 시간 row와 측정소번호 column으로 pivot_wider를 할 수 있음을 확인
+#문제는 각각의 항목을 대상으로 각각 pivot_wider를 해야 하는 것이 힘들어보임
+#다시 처음으로 돌아와서 초기 raw데이터에서 바로 타겟 데이터를 작성하는 구문 작성 시도
+raw_1904_1910_7 <- mutate(raw_1904_1910_6, t_PM10 = 0, t_PM2.5 = 0) #일단 타겟데이터 초기값을 0으로 설정해서 컬럼 선언
+
+#loc_code도 맞아야함 but 시간이 너무 오래걸림
+for(i in 1:length(raw_1904_1910_7$DATA_DT)){
+  for(j in 1:length(raw_1904_1910_7$DATA_DT)){
+    if(raw_1904_1910_7$DATA_DT[i] + 10000 == raw_1904_1910_7$DATA_DT[j]){
+      raw_1904_1910_7$t_PM10[i] <- raw_1904_1910_7$PM10[j]
+      raw_1904_1910_7$t_PM2.5[i] <- raw_1904_1910_7$PM2.5[j]
+    }
+  }
+}
